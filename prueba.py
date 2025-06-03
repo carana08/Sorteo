cf6d04d71c4fc73303efe5e4a90b134ffc489d2b
César Arana <carana@espol.edu.ec>
2025-06-02 21:57:37 -0500

    Actualizando función

--- prueba.py
+++ prueba.py
@@ -2,10 +2,12 @@ import random as rd
 
 def sorteo(lista1, lista2):
     diccionario = {}
-    primera_eleccion_aleatoria = rd.randint(0, len(lista1) - 1)
-    segunda_eleccion_aleatoria = rd.randint(0, len(lista2) - 1)
-    diccionario.update({lista1[primera_eleccion_aleatoria]: lista2[segunda_eleccion_aleatoria]})
-    
-    return lista1[numero_aleatorio], lista2[numero_aleatorio]
+    for i in range(len(lista1)):
+        primera_eleccion_aleatoria = rd.randint(0, len(lista1) - 1)
+        segunda_eleccion_aleatoria = rd.randint(0, len(lista2) - 1)
+        diccionario.update({lista1[primera_eleccion_aleatoria]: lista2[segunda_eleccion_aleatoria]})
+        lista1.pop(primera_eleccion_aleatoria)
+        lista2.pop(segunda_eleccion_aleatoria)
+    return diccionario
 
-print(sorteo())
+print(sorteo(['Jose', 'Juan', 'Carlos'], ['250', '78', '90']))
